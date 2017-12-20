559a28a2dee6b6723a41fd23c1082bf6d4ede002
Karthik M S <ms18karthik@gmail.com>
2017-12-20 22:47:15 +0530

    Added comments to the post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,12 +4,38 @@ class CommentsController < ApplicationController
 
 		@post = Post.friendly.find(params[:post_id])
 		@comment = @post.comments.build(params[:comment].permit(:comment))
+		@comment.user_id = current_user.id if current_user
+		@comment.save
 
 		if @comment.save
 			redirect_to post_path(@post)
 		else
 			render 'new'
 		end
+	end
+
+	def edit
+		@post = Post.friendly.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+	end
+
+	def update
+		@post = Post.friendly.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+
+		if @comment.update(params[:comment].permit(:comment))
+			redirect_to post_path(@post)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy	
+		@post = Post.friendly.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
+		redirect_to post_path(@post)
+
 	end
 
 end
